abc3201c14e59a1836b242d224bf17c58edf6c8c
Kirill <187307451+shirmanovka@users.noreply.github.com>
2025-01-23 12:59:55 +0500

    Update terminal_app.py

--- terminal_app.py
+++ terminal_app.py
@@ -487,12 +487,99 @@ def dcm_Currency_Indexes_Swap():
         if st.button('Обновить данные', key='refresh'):
             st.script_runner.rerun()
 
-
+def dcm_Currency_Indexes_Swap():
+    
+    st.header("Listing application")
+  
+    
+    # Заголовки для запроса
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    
+    # Функция для скачивания файла и чтения данных
+    def download_data():
+        url = 'https://web.moex.com/moex-web-icdb-api/api/v1/export/listing-applications/xlsx'
+        response = requests.get(url, headers=headers)
+    
+        if response.status_code == 200:
+            with open('listing_applications.xlsx', 'wb') as f:
+                f.write(response.content)
+            return pd.read_excel('listing_applications.xlsx')
+        else:
+            st.error("Ошибка при скачивании данных.")
+            return None
+    
+    # Заголовок приложения
+    st.title("Скачивание данных о заявках на листинг")
+    
+    # Кнопка для скачивания данных
+    if st.button("Скачать данные"):
+        df = download_data()
+        if df is not None:
+            columns_to_keep = [
+            'Наименование эмитента',
+            'ИНН эмитента',
+            'Категория(тип) ценной бумаги',
+            'Идентификатор выпуска*',
+            'Уровень листинга',
+            'Дата получения заявления',
+            'Дата раскрытия информации'
+        ]
+            df = df[columns_to_keep]
+            df['Категория(тип) ценной бумаги'] = df['Категория(тип) ценной бумаги'].str.replace(' ', '\n')
+            
+            abbreviation = {
+           'Общество с ограниченной ответственностью': 'OOO',
+           'Публичное акционерное общество': 'ПAO',
+           'Акционерное общество': 'AO'
+            }
+    
+       # Замена полных названий на аббревиатуры
+            df['Наименование эмитента'] = df['Наименование эмитента'].replace(abbreviation)
+            
+            def highlight_rows(row):
+                return ['background-color: lightgreen' if row['Идентификатор выпуска*'] == "Не присвоен" else '' for _ in row]
+    
+            new_df = df[df['Идентификатор выпуска*'] == "Не присвоен"]
+            new_df = new_df[['Наименование эмитента', 'Категория(тип) ценной бумаги', 'Идентификатор выпуска*']].tail(5)
+    
+            styled_new_df = new_df.style.apply(highlight_rows, axis=1)  # Применение стиля к новому DataFrame
+            
+            df = df.tail(10)
+            
+            styled_df = df.style.apply(highlight_rows, axis=1)
+    
+    
+            for full_name, abbreviation in abbreviation.items():
+                df['Наименование эмитента'] = df['Наименование эмитента'].replace(full_name, abbreviation, regex=True)
+            
+            styled_df = df.style.apply(highlight_rows, axis=1)    
+                # Вывод последних 5 строк DataFrame с примененной стилизацией
+                # Новый DataFrame для условия "Не присвоен"
+    
+            
+            st.markdown("""
+                <style>
+                    .dataframe th, .dataframe td {
+                        padding: 5px;
+                        text-align: left;
+                        font-size: 12px;
+                        white-space: pre-wrap; /* Позволяет переносить текст */
+                    }
+                </style>
+            """, unsafe_allow_html=True)
+    
+            
+            st.dataframe(styled_df.format({'ИНН эмитента': '{:.0f}'}), use_container_width=True)  # Основной DataFrame
+            
+            st.subheader("Последние заявки эмитентов на регистрацию нового выпуска")
+            st.dataframe(styled_new_df, use_container_width=True)  # Новый DataFrame
 
 
 def main():
     st.sidebar.title("DCM analytical terminal")
-    menu = st.sidebar.selectbox("Выберите приложение", ("Pricing spread", "Matchbox", "Maturity volume","Currency, Indexes, Swap Curve"))
+    menu = st.sidebar.selectbox("Выберите приложение", ("Pricing spread", "Matchbox", "Maturity volume","Currency, Indexes, Swap Curve", "Listing application"))
 
     if menu == "Pricing spread":
         dcm_pricing_spread()
@@ -502,6 +589,8 @@ def main():
         dcm_Mat_val()
     elif menu == "Currency, Indexes, Swap Curve":
         dcm_Currency_Indexes_Swap()
+    elif menu == "Listing application":
+        dcm_Listing_application()    
 
 if __name__ == "__main__":
     main()
